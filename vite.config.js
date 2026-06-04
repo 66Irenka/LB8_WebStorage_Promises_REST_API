a96931d328d5942055c1cd2c9ea59fff2ab20bd7
66Irenka <pavlenkoiren66@gmail.com>
2026-06-04 09:49:57 +0300

    fix responsive styles for image search page

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/LB8_WebStorage_Promises_REST_API/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        timer: resolve(__dirname, 'timer.html'),
+        form: resolve(__dirname, 'form.html'),
+        snackbar: resolve(__dirname, 'snackbar.html'),
+        gallery: resolve(__dirname, 'gallery.html'),
+        search: resolve(__dirname, 'search.html'),
+      },
+    },
+  },
 });
